069eca5d77c1610fbba2b8c3bd2e74ee64fb336a
thsonu35 <thsonu350@gmail.com>
2024-07-03 18:21:54 +0530

    completed

--- pro manage/src/App.jsx
+++ pro manage/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/share/:id" element={<Sharepage/>}/>
 
