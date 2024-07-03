38c491ddd6995ff4589295b8bde3a0be108f0611
thsonu35 <thsonu350@gmail.com>
2024-07-03 20:53:20 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const errorStream = fs.createWriteStream(path.join(__dirname, 'error.txt'), { fl
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
