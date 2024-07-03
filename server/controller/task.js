ee992f5fea4586d34614a5ff2c404671ccd041de
thsonu35 <thsonu350@gmail.com>
2024-07-03 22:55:35 +0530

    completed

--- server/controller/task.js
+++ server/controller/task.js
@@ -1,5 +1,5 @@
 const Task = require('../models/Task'); 
-const User = require('../models/User'); 
+const User = require('../models/user'); 
 
 const createTask = async (req, res, next) => {
     try {
--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const bcrypt = require('bcrypt')
 
 const registerUser = async (req, res) => {
